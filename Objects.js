0fc5a28b8e50c3924de67f6d65721464519aab37
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-05-15 13:17:36 -0700

    prototype

--- Objects.js
+++ Objects.js
@@ -4,17 +4,17 @@
 //delimiter?
 
 //implement a method interpolate
-var spanishNumbers = ['uno', 'do', 'tres', 'catorce'];
-console.log(spanishNumbers);
-spanishNumbers.interpolate = function(delimiter) {
-	var combineString = '';
-	for (var i = 0; i < this.length; i++) {
-		combineString += this[i] + delimiter;
-	};
-	combineString += this[this.length - 1]
-	return combineString;
-};
-console.log(spanishNumbers.interpolate(', '));
+// var spanishNumbers = ['uno', 'do', 'tres', 'catorce'];
+// console.log(spanishNumbers);
+// spanishNumbers.interpolate = function(delimiter) {
+// 	var combineString = '';
+// 	for (var i = 0; i < this.length; i++) {
+// 		combineString += this[i] + delimiter;
+// 	};
+// 	combineString += this[this.length - 1]
+// 	return combineString;
+// };
+// console.log(spanishNumbers.interpolate(', '));
 
 
 //constuctor: a function that creates and returns a new object
@@ -25,10 +25,11 @@ console.log(spanishNumbers.interpolate(', '));
 //new when you call MyArray with new before it, js creates a new js object and assign this to that object
 
 var MyArray = function () {
+  var toReturn = [];
   for (var i = 0; i < arguments.length; i++) {
-    this[i] = arguments[i];
+    toReturn[i] = arguments[i];
   }
-  this.interpolate = function(delimiter) {
+  toReturn.interpolate = function(delimiter) {
     var joined = ""
     for (var i = 0; i < this.length - 1; i++) {
       joined += this[i] + delimiter;
@@ -36,6 +37,25 @@ var MyArray = function () {
     joined += this[this.length - 1];
     return joined;
   };
+  return toReturn;
+  console.log(toReturn);
 }
 
-var someArray = new MyArray(1, 2, 3, 4, 5);
+
+// var MyArray = function () {
+//   for (var i = 0; i < arguments.length; i++) {
+//     this[i] = arguments[i];
+//   }
+//   this.interpolate = function(delimiter) {
+//     var joined = ""
+//     for (var i = 0; i < this.length - 1; i++) {
+//       joined += this[i] + delimiter;
+//     }
+//     joined += this[this.length - 1];
+//     return joined;
+//   };
+// }
+
+// var someArray = new MyArray(1, 2, 3, 4, 5);
+
+// console.log(someArray);
