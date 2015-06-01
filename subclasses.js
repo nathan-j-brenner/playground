38770a0f82c370a1f5b158fb896583082c97ca27
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-06-01 16:02:50 -0700

    exercise 2 and 3 solutions

--- subclasses.js
+++ subclasses.js
@@ -1,51 +1,52 @@
-function Deque() {
-  this.arr = new Array();
-}
+// function Deque() {
+//   this.arr = new Array();
+// }
 
-  Deque.prototype.push = function (item) {
-    return this.push(item)
-  }
-  //add an element to the end of an thisay, returns the length of the array
+//   Deque.prototype.push = function (item) {
+//     return this.push(item)
+//   }
+//   //add an element to the end of an thisay, returns the length of the array
 
-  Deque.prototype.pop = function () {
-    return this.pop()
-  }
-  //removes the last element from an array, returns that element
+//   Deque.prototype.pop = function () {
+//     return this.pop()
+//   }
+//   //removes the last element from an array, returns that element
 
-  Deque.prototype.unshift = function (item) {
-    return this.unshift(item)
-  }
-  //add an element to the beginning of an array, returns the length of the array
+//   Deque.prototype.unshift = function (item) {
+//     return this.unshift(item)
+//   }
+//   //add an element to the beginning of an array, returns the length of the array
 
-  Deque.prototype.shift = function () {
-    return this.shift()
-  }
-  //removes the first element from an array and returns that element
-}
+//   Deque.prototype.shift = function () {
+//     return this.shift()
+//   }
+//   //removes the first element from an array and returns that element
+// }
 
-var Queue = (function(){
-	function Queue(){
-		Deque.call(this);
-	}
-	// var proto = new Deque([]);
-	// proto.constructor = Queue;
-	var  proto = Object.create(Deque.prototype);
+// var Queue = (function(){
+// 	function Queue(){
+// 		Deque.call(this);
+// 	}
+// 	// var proto = new Deque([]);
+// 	// proto.constructor = Queue;
+// 	var  proto = Object.create(Deque.prototype);
 
-	//proto.push adds vals to the end of an array
-	proto.pop = undefined; //this would remove the last item of the array
-	proto.unshift = undefined; //this would add an element to the beginning of an array
-	//proto.shift = true; removes first element of the array
+// 	//proto.push adds vals to the end of an array
+// 	proto.pop = undefined; //this would remove the last item of the array
+// 	proto.unshift = undefined; //this would add an element to the beginning of an array
+// 	//proto.shift = true; removes first element of the array
 
-	return Queue;
-})();
+// 	return Queue;
+// })();
 
-var queue = new Queue();
-queue.push(1);
-queue.push(2);
-queue.push(3);
-console.log(queue.shift());
-console.log(queue.unshift());
+// var queue = new Queue();
+// queue.push(1);
+// queue.push(2);
+// queue.push(3);
+// console.log(queue.shift());
+// console.log(queue.unshift());
 
+//basics of inheritance
 function Ctor() {
 	this.a = 0;
 	this.b = 1;
@@ -61,7 +62,80 @@ obj1.c = 2;
 //Ctor.prototype access the prototype from the constructor
 //obj1.__proto__ ugly, not really in javaScript
 obj1.constructor.prototype.d = 3;
-//every instance of Ctor has d: 3 because they are located in the prototype of Ctor
+//every instance of Ctor has the property d: 3 because they are located in the prototype of Ctor, which is also the proto of each instance of Ctor
+
+//modifying prototypes
+function A(){}; //create new constructor.  A is an empty object.
+A.prototype = {
+	num: 0,
+	str: 'default'
+}
+//prototype of A constructor has those properties
+var objA = new A();
+//objA.constructor.prototype is the prototype of the Object constructor, not the A constructor
+
+
+function B(){}; //same as A constructor
+B.prototype.num = 0;
+B.prototype.str = 'default';
+var objB = new B();
+
+//Imaginary Menagerie
+function Animal(name){
+	this.name = name;
+}
+
+Animal.prototype.move = 'walk';
+
+function Bird(name){
+	Animal.call(this, name);
+}
+
+Bird.prototype = Object.create(Animal.prototype);
+Bird.prototype.constructor = Bird;
+
+Bird.prototype.move = 'fly';
+Bird.prototype.hasWings = true;
+
+function Fish(name){
+	Anima.call(this, name);
+}
+Fish.prototype = Object.create(Animal.prototype);
+Fish.prototype.constructor = Fish;
+
+Fish.prototype.move = 'swim';
+
+function Penguin(name){
+	Bird.call(this, name);
+}
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+Penguin.prototype.move = Fish.prototype.move;
+
+new Animal("Simba").move;// 'walk'
+new Fish("Nemo").move;// 'swim'
+new Bird("Lulu").move;// 'fly'
+var pengo = new Penguin("Pengo");
+console.log(pengo.name);     // "Pengo"
+console.log(pengo.move);   //'swim'
+console.log(pengo.hasWings); //true;
+console.log(pengo instanceof Penguin); //true
+console.log(pengo instanceof Bird);      //true
+console.log(pengo instanceof Animal);  //true
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
